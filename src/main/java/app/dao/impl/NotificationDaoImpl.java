0d325da9348400c881840ea6d32aa368f5b9c9fa
Ilia G <ilia119@tut.by>
2019-05-06 21:03:49 +0200

    Refactoring

--- src/main/java/app/dao/impl/NotificationDaoImpl.java
+++ src/main/java/app/dao/impl/NotificationDaoImpl.java
@@ -15,68 +15,68 @@ import java.util.NoSuchElementException;
 @Transactional
 public class NotificationDaoImpl implements BasicCrudDao<Notification> {
 
-	@Autowired
-	private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-	@Override
-	public void create(Notification notification) {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    session.save(notification);
-	    session.getTransaction().commit();
-	    session.close();
-	}
+    @Override
+    public void create(Notification notification) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(notification);
+        session.getTransaction().commit();
+        session.close();
+    }
 
-	@Override
-	public void update(Notification notification) {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    session.update(notification);
-	    session.getTransaction().commit();
-	    session.close();
-	}
+    @Override
+    public void update(Notification notification) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(notification);
+        session.getTransaction().commit();
+        session.close();
+    }
 
-	@Override
-	public Notification findById(int id) {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    Notification notification = session.get(Notification.class, id);
-	    if (notification == null) {
-	        throw new NoSuchElementException("No such notification");
+    @Override
+    public Notification findById(int id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Notification notification = session.get(Notification.class, id);
+        if (notification == null) {
+            throw new NoSuchElementException("No such notification");
         }
-	    session.getTransaction().commit();
-	    session.close();
-		return notification;
-	}
+        session.getTransaction().commit();
+        session.close();
+        return notification;
+    }
 
-	@Override
-	public List<Notification> findAll() {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    List<Notification> notifications = session.createQuery("from Notification").getResultList();
-	    session.getTransaction().commit();
-	    session.close();
-		return notifications;
+    @Override
+    public List<Notification> findAll() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<Notification> notifications = session.createQuery("from Notification").getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return notifications;
 
-	}
+    }
 
-	@Override
-	public void deleteById(int id) {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    session.delete(findById(id));
-	    session.getTransaction().commit();
-	    session.close();
-	}
+    @Override
+    public void deleteById(int id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(findById(id));
+        session.getTransaction().commit();
+        session.close();
+    }
 
-	@Override
-	public void delete(Notification notification) {
-	    Session session = sessionFactory.openSession();
-	    session.beginTransaction();
-	    session.delete(notification);
-	    session.getTransaction().commit();
-	    session.close();
-	}
+    @Override
+    public void delete(Notification notification) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(notification);
+        session.getTransaction().commit();
+        session.close();
+    }
 
 
 }
