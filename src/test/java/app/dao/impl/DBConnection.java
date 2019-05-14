cbf096d5c99282ab93ac6c2e6ec5ee1618615c10
Ilia <ilia119@tut.by>
2019-05-14 11:53:13 +0300

    Refactoring

--- src/test/java/app/dao/impl/DBConnection.java
+++ src/test/java/app/dao/impl/DBConnection.java
@@ -9,30 +9,29 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    @Value("${jdbc.driver}")
-    private static String driver = "com.mysql.cj.jdbc.Driver";
-    @Value("${db.url}")
-    private static String url = "jdbc:mysql://localhost:3306/timesheet_dev?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    @Value("${db.username}")
-    private static String username = "username";
-    @Value("${db.password}")
-    private static String password = "qwerty123";
+	@Value("${jdbc.driver}")
+	private static String driver = "com.mysql.cj.jdbc.Driver";
+	@Value("${db.url}")
+	private static String url = "jdbc:mysql://localhost:3306/timesheet_dev?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	@Value("${db.username}")
+	private static String username = "username";
+	@Value("${db.password}")
+	private static String password = "qwerty123";
 
 
-    public Connection getConnection() {
-        try {
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
+	public Connection getConnection() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			return DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 
 }
